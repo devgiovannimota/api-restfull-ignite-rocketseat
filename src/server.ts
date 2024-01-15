d531da1399fae2b905d317b784392b6ad8b18efd
Giovanni <devgiovannimota@gmail.com>
2024-01-15 13:48:39 -0300

    Created POST end point

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import fastify from "fastify";
-import { knex } from "./database";
 import { env } from "./env";
+import { transactionRoutes } from "./routes/transactions";
 
 const app = fastify();
+app.register(transactionRoutes, {
+  prefix: "transactions",
+});
 
 app
   .listen({
